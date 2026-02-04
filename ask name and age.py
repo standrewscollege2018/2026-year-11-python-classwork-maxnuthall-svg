e6f33e4e7ee240acdf184fcbeb79d217079fdd12
maxnuthall-svg <maxnuthall@outlook.com>
2026-02-05 12:34:35 +1300

    first input sequence

---
+++ ask name and age.py
@@ -0,0 +1,7 @@
+print(f"My name is {name} and I am {age} years old")
+
+#we use input() to get input from the user
+user_name = input("What is your name? ")
+print(f"Hello {user_name}")
+
+user_age = input(f"how old are you {user_name}?")
