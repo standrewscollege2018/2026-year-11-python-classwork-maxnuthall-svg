1a127dfe75eb94cbad47bfa6b1b0eb2060bf01b2
maxnuthall-svg <maxnuthall@outlook.com>
2026-02-12 12:08:36 +1300

    Update paracitomal dosage.py

--- paracitomal dosage.py
+++ paracitomal dosage.py
@@ -2,6 +2,11 @@
 #get age and weight from user
 weight = int(input("Enter your weight "))
 age = int(input("Enter your age: "))
-
-if age < 9 and weight <= 40:
-    print(" you should take 400g of paracitomal")
+if age >= 0 and weight >= 0:
+    if age <= 2:
+        print(" you should take 2 500mg tables")
+    else:
+        weight = weight * 10
+        print ("you should take " + str (weight) + "mg")
+else:   
+    print ("this isnt a valid input")
