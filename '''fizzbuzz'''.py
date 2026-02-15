7750542470687f56d57896c0490103bb88281463
maxnuthall-svg <maxnuthall@outlook.com>
2026-02-16 11:13:26 +1300

    Create '''fizzbuzz'''.py

---
+++ '''fizzbuzz'''.py
@@ -0,0 +1,10 @@
+'''fizzbuzz'''
+for number in range(1,31):
+    if number %3 and %5 == 0::
+        print ("fizzbuzz")
+    elif number %3 == 0:
+        print("fizz")
+    elif number %5 == 0:
+        print ("buzz")
+    else:
+        print (number)
