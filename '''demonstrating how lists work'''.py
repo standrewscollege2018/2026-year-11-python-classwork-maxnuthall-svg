7fece5038a58d1bcd076640016818643559981be
maxnuthall-svg <maxnuthall@outlook.com>
2026-02-23 11:00:25 +1300

    Create '''demonstrating how lists work'''.py

---
+++ '''demonstrating how lists work'''.py
@@ -0,0 +1,7 @@
+'''demonstrating how lists work'''
+#list are used to store multiple peices of informtion
+#We use square brackets to show its a list
+names = ["Ace", "chopper", "luffy", "nami", "sanji", "ussop", "zoro"]
+
+#print the entire list
+print(names)
